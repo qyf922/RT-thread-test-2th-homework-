a13ffa0e33bb13cc2186b5b52f378a9ffaa9a7f4
qyf922 <1192894647@qq.com>
2022-05-14 23:41:54 +0800

    实验3

--- applications/main.c
+++ applications/main.c
@@ -7,61 +7,58 @@
  * Date           Author       Notes 
  * 2018-08-24     yangjie      the first version 
  */ 
- 
- /*
- * 程序清单：相同优先级线程按照时间片轮番调度
- *
- * 这个例子中将创建两个线程，每一个线程都在打印信息
- * 
- */
 
+/*
+* 程序清单：定时器例程
+*
+* 这个例程会创建两个动态定时器，一个是单次定时，一个是周期性定时
+* 并让周期定时器运行一段时间后停止运行
+*/
 #include <rtthread.h>
 
-#define THREAD_STACK_SIZE	1024
-#define THREAD_PRIORITY	    20
-#define THREAD_TIMESLICE    10
+/* 定时器的控制块 */
+static rt_timer_t timer1;
+static rt_timer_t timer2;
+static int cnt = 0;
 
-/* 线程入口 */
-static void thread_entry(void* parameter)
+/* 定时器1超时函数 */
+static void timeout1(void *parameter)
 {
-    rt_uint32_t value;
-    rt_uint32_t count = 0;
+    rt_kprintf("periodic timer is timeout %d\n", cnt);
 
-    value = (rt_uint32_t)parameter;
-    while (1)
+    /* 运行第10次，停止周期定时器 */
+    if (cnt++ >= 9)
     {
-        if(0 == (count % 5))
-        {           
-            rt_kprintf("thread %d is running ,thread %d count = %d\n", value , value , count);      
-
-            if(count > 200)
-                return;            
-        }
-         count++;
-     }  
+        rt_timer_stop(timer1);
+        rt_kprintf("periodic timer was stopped! \n");
+    }
 }
 
-int timeslice_sample(void)
+/* 定时器2超时函数 */
+static void timeout2(void *parameter)
 {
-    rt_thread_t tid = RT_NULL;
-    /* 创建线程1 */
-    tid = rt_thread_create("thread1", 
-                            thread_entry, (void*)1, 
-                            THREAD_STACK_SIZE, 
-                            THREAD_PRIORITY, THREAD_TIMESLICE); 
-    if (tid != RT_NULL) 
-        rt_thread_startup(tid);
+    rt_kprintf("one shot timer is timeout\n");
+}
 
-    /* 创建线程2 */
-    tid = rt_thread_create("thread2", 
-                            thread_entry, (void*)2,
-                            THREAD_STACK_SIZE, 
-                            THREAD_PRIORITY, THREAD_TIMESLICE-5);
-    if (tid != RT_NULL) 
-        rt_thread_startup(tid);
-        
+int timer_sample(void)
+{
+    /* 创建定时器1  周期定时器 */
+    timer1 = rt_timer_create("timer1", timeout1,
+                             RT_NULL, 10,
+                             RT_TIMER_FLAG_PERIODIC);
+
+    /* 启动定时器1 */
+    if (timer1 != RT_NULL) rt_timer_start(timer1);
+
+    /* 创建定时器2 单次定时器 */
+    timer2 = rt_timer_create("timer2", timeout2,
+                             RT_NULL,  30,
+                             RT_TIMER_FLAG_ONE_SHOT);
+
+    /* 启动定时器2 */
+    if (timer2 != RT_NULL) rt_timer_start(timer2);
     return 0;
 }
 
 /* 导出到 msh 命令列表中 */
-MSH_CMD_EXPORT(timeslice_sample, timeslice sample);
+MSH_CMD_EXPORT(timer_sample, timer sample);
